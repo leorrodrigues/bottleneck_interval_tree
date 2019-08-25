e39cfb4d8ccca05ac827c262fd0f73639dd1eb07
leonardo <leu_rr1@hotmail.com>
2019-08-25 02:29:06 -0300

    test tool

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,40 @@
 RED='\033[0;31m |-'
 BLUE='\033[0;34m |-'
+YELLOW='\033[1;33m |-'
 NC='\033[0m'
+FILES=$(ls -l tests/ | grep  'case' | grep -v 'out' | grep -v 'valgrind' | awk '{print $9}')
 
-FILES=$(ls -l tests/ | grep  'case' | grep -v 'out' | awk '{print $9}')
+PROGRAM_NAME="./$1"
 
+if [ -z "$1" ]
+then
+    PROGRAM_NAME="./interval.out"
+fi
+
+#interval.out
 reset && make && {
+    echo "Program Tests\n";
     for i in $FILES; do
-    ./interval.out < tests/$i > tests/out_$i;
-    cmp tests/result$(echo "${i#case}") tests/out_$i && printf "${BLUE}Teste $i Passou ${NC}\n" && rm tests/out_$i || printf "${RED}Teste $i Falhou${NC}\n";
-done;} || printf "${RED} Erro na compilação${NC}\n"
+        $PROGRAM_NAME < tests/$i > tests/out_$i;
+        cmp tests/result$(echo "${i#case}") tests/out_$i && printf "${BLUE}Test $i [x] ${NC}\n" && rm tests/out_$i || printf "${RED}Test $i []${NC}\n";
+    done;
+
+    echo "Running Tests\n";
+    if hash valgrind >/dev/null;
+    then
+        for i in $FILES; do
+            valgrind --leak-check=full --error-exitcode=121 --log-file="tests/valgrind_$i" $PROGRAM_NAME < tests/$i > tests/out_$i
+            if [ $? -eq 0 ]
+            then
+                printf "${BLUE}Memory Test $i [x] ${NC}\n";
+                rm tests/out_$i tests/valgrind_$i;
+            else
+                 printf "${YELLOW}Memory Test $i [] (check the logs in /tests/ folder)${NC}\n";
+            fi;
+        done;
+        rm -r vgcore*
+    else
+        print "${RED} Need to install Valgrind${NC}\n";
+    fi
+
+} || printf "${RED} Erro na compilação${NC}\n"
